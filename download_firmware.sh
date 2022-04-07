6d2b74e80444129670854dce1b4cc62b602eb6a8
Duncan V Nicholson <dvn5207@psu.edu>
2022-04-07 18:02:08 -0400

    disable logging

--- download_firmware.sh
+++ download_firmware.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 cd ~/zmk
-rm -v tightyl_left-nrfmicro_11-zmk.uf2 tightyl_right-nrfmicro_11-zmk.uf2
+rm -v tightyl_left-nrfmicro_11-zmk.uf2 tightyl_right-nrfmicro_11-zmk.uf2 settings_reset-nrfmicro_11-zmk.uf2
 mv -v ~/Downloads/firmware.zip .
 unzip firmware.zip
 rm -v firmware.zip
